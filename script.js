2c7a804bfe3036de9984a1dccc668e86ece1ece7
TGGavin <178350968+TGGavin@users.noreply.github.com>
2025-01-30 21:37:33 -0800

    made loop to create 256 divs (16 x 16)

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("You should study for your drivers test"); //fuck
+
+// figure how to make the divs into a 16 x 16 grid. one found method is max-width max height, but it feels cheatsies.
+function divGridMaker() {
+const container = document.querySelector("#container")
+
+// 16 x 16 = 256
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div")
+    div.classList.add("div-block")
+    container.appendChild(div)
+    }
+}
+divGridMaker()
