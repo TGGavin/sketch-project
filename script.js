ec4070bf6f79052ed06336e0b9b1f2c7a2ba52fe
TGGavin <178350968+TGGavin@users.noreply.github.com>
2025-02-11 14:51:58 -0800

    added If conditon in order to only color rgb-square class

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-console.log("You should study for your drivers test"); //fuck
 
 // figure how to make the divs into a 16 x 16 grid. one found method is max-width max height, but it feels cheatsies.
-function divGridMaker() {
-const gridContainer = document.querySelector("#container")
-
+function createTable() {
+const gridContent = document.querySelector("#eas-content")
 // 16 x 16 = 256
+// Will need to change loop iterator, and splice with prompt input
+// "eas" means "etch-a-sketch"
+
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div")
-    div.classList.add("div-block")
-    gridContainer.appendChild(div)
+    div.classList.add("rgb-square")
+    gridContent.appendChild(div)
     }
+
+    gridContent.addEventListener("mouseover", (e) => {
+        target = e.target
+        if (target.classList.contains("rgb-square")) {
+        target.setAttribute("style", "background-color: red;")
+        } else {
+            console.log(`${target.id} Is not intented to colored`)
+        }
+    })
 }
-divGridMaker()
+createTable()
